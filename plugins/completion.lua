2e48be473b184d86e14dfd889a82413c5b3c64ee
mjkx <291083247@qq.com>
2024-04-07 03:07:42 +0800

    add nvim-lsp to override for add user snips.

---
+++ plugins/completion.lua
@@ -0,0 +1,35 @@
+local completion_tool = {}
+
+completion_tool["hrsh7th/nvim-cmp"] = {
+	lazy = true,
+	event = "InsertEnter",
+	config = require("completion.cmp"),
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = { "rafamadriz/friendly-snippets" },
+			config = require("configs.completion.luasnip"),
+		},
+		{ "lukas-reineke/cmp-under-comparator" },
+		{ "saadparwaiz1/cmp_luasnip" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-nvim-lua" },
+		{ "andersevenrud/cmp-tmux" },
+		{ "hrsh7th/cmp-path" },
+		{ "f3fora/cmp-spell" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "kdheepak/cmp-latex-symbols" },
+		{ "ray-x/cmp-treesitter", commit = "c8e3a74" },
+		-- { "tzachar/cmp-tabnine", build = "./install.sh", config = require("completion.tabnine") },
+		-- {
+		-- 	"jcdickinson/codeium.nvim",
+		-- 	dependencies = {
+		-- 		"nvim-lua/plenary.nvim",
+		-- 		"MunifTanjim/nui.nvim",
+		-- 	},
+		-- 	config = require("completion.codeium"),
+		-- },
+	},
+}
+
+return completion_tool
