48ade52e80473e9b2cdc102dfaf93c9ccd8156e4
mjkx <291083247@qq.com>
2024-04-10 13:53:12 +0800

    accelerate h, l, e, b

--- configs/editor/accelerated-jk.lua
+++ configs/editor/accelerated-jk.lua
@@ -2,7 +2,7 @@ return function()
 	require("modules.utils").load_plugin("accelerated-jk", {
 		mode = "time_driven",
 		enable_deceleration = false,
-		acceleration_motions = {},
+		acceleration_motions = { "h", "l", "e", "b" },
 		acceleration_limit = 150,
 		acceleration_table = { 7, 12, 17, 21, 24, 26, 28, 30 },
 		-- when 'enable_deceleration = true', 'deceleration_table = { {200, 3}, {300, 7}, {450, 11}, {600, 15}, {750, 21}, {900, 9999} }'
