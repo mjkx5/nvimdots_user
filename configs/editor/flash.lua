59423831110e71f176a13eda224eb72931887bd0
mjkx <291083247@qq.com>
2024-04-17 13:28:35 +0800

    disable t T , ; in flash search

--- configs/editor/flash.lua
+++ configs/editor/flash.lua
@@ -22,6 +22,7 @@ return function()
 			-- options used when flash is activated through
 			-- `f`, `F`, `t`, `T`, `;` and `,` motions
 			char = {
+				keys = { "f", "F" },
 				enabled = true,
 				-- hide after jump when not using jump labels
 				autohide = false,
