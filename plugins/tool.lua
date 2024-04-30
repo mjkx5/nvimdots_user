2731f35ae304ef19c2011c8432d81e371f5b80ce
mjkx <291083247@qq.com>
2024-05-01 01:57:43 +0800

    add config on fcitx5

--- plugins/tool.lua
+++ plugins/tool.lua
@@ -4,7 +4,7 @@ user_tool["pysan3/fcitx5.nvim"] = {
 	lazy = true,
 	event = "BufReadPost",
 	cond = vim.fn.executable("fcitx5-remote") == 1,
-	config = require("tool.fcitx5"),
+	config = require("configs.tool.fcitx5"),
 }
 
 user_tool["skywind3000/asynctasks.vim"] = {
