f4c90ecda7ea56a43ee7107afba1299fff086a6c
mjkx <291083247@qq.com>
2024-05-08 16:07:54 +0800

    add some omap, w->W, e->E, b->B, W->w, E->e, B->b

--- keymap/editor.lua
+++ keymap/editor.lua
@@ -1,6 +1,7 @@
 local bind = require("keymap.bind")
 local map_callback = bind.map_callback
 local et = bind.escape_termcode
+local map_cmd = bind.map_cmd
 
 return {
 
@@ -11,4 +12,11 @@ return {
 	["n|k"] = map_callback(function()
 		return et("<Plug>(accelerated_jk_gk)")
 	end):with_expr(),
+
+	["o|w"] = map_cmd("W"):with_noremap():with_silent():with_desc(""),
+	["o|W"] = map_cmd("w"):with_noremap():with_silent():with_desc(""),
+	["o|e"] = map_cmd("E"):with_noremap():with_silent():with_desc(""),
+	["o|E"] = map_cmd("e"):with_noremap():with_silent():with_desc(""),
+	["o|b"] = map_cmd("B"):with_noremap():with_silent():with_desc(""),
+	["o|B"] = map_cmd("b"):with_noremap():with_silent():with_desc(""),
 }
