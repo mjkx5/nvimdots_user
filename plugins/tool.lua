98f1a30ff88798b3b5169c2222fdb7d3eb545c93
mjkx <291083247@qq.com>
2024-05-18 04:09:17 +0800

    update fcitx5 switch plugin

--- plugins/tool.lua
+++ plugins/tool.lua
@@ -1,10 +1,38 @@
 local user_tool = {}
 
-user_tool["pysan3/fcitx5.nvim"] = {
+user_tool["keaising/im-select.nvim"] = {
 	lazy = true,
-	event = "BufReadPost",
 	cond = vim.fn.executable("fcitx5-remote") == 1,
-	config = require("configs.tool.fcitx5"),
+	event = "BufReadPost",
+	config = function()
+		require("im_select").setup({
+			-- IM will be set to `default_im_select` in `normal` mode
+			-- For Windows/WSL, default: "1033", aka: English US Keyboard
+			-- For macOS, default: "com.apple.keylayout.ABC", aka: US
+			-- For Linux, default:
+			--               "keyboard-us" for Fcitx5
+			--               "1" for Fcitx
+			--               "xkb:us::eng" for ibus
+			-- You can use `im-select` or `fcitx5-remote -n` to get the IM's name
+			default_im_select = "keyboard-us",
+
+			-- Can be binary's name or binary's full path,
+			-- e.g. 'im-select' or '/usr/local/bin/im-select'
+			-- For Windows/WSL, default: "im-select.exe"
+			-- For macOS, default: "im-select"
+			-- For Linux, default: "fcitx5-remote" or "fcitx-remote" or "ibus"
+			default_command = "fcitx5-remote",
+
+			-- Restore the default input method state when the following events are triggered
+			set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+
+			-- Show notification about how to install executable binary when binary missed
+			keep_quiet_on_no_binary = false,
+
+			-- Async run `default_command` to switch IM or not
+			async_switch_im = true,
+		})
+	end,
 }
 
 user_tool["skywind3000/asynctasks.vim"] = {
